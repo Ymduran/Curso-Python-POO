e114649919282d2117c951897b2f4920e0007334
Duran Breceda <romeromatias273@gmail.com>
2025-03-10 16:10:18 -0600

    clases

--- Clases_y_objetos/Atributos_de_instancia.py
+++ Clases_y_objetos/Atributos_de_instancia.py
@@ -9,38 +9,58 @@ print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
 print(" ")
 
-
+# Definición de la clase Estudiante
 class Estudiante:
-    def __init__(self,
-                 nombre: str,
-                 temas_aprendidos: list[str]
-                 ):
-        def __init__(self, nombre):
-            self.nombre = nombre
-            self.temas_aprendidos = []
+    def __init__(self, nombre: str, temas_aprendidos: list[str] = None):
+        self.nombre = nombre
+        self.temas_aprendidos = temas_aprendidos if temas_aprendidos is not None else []
 
-        def aprender_tema(self, tema:str) ->None:
-            self.temas_aprendidos.append(tema)
-            print(f"{self.nombre} aprendió {tema}")
+    def aprender_tema(self, tema: str) -> None:
+        self.temas_aprendidos.append(tema)
+        print(f"{self.nombre} aprendió {tema}")
+    def __str__(self) -> str:
+        return f"Estudiante(nombre: {self.nombre}, temas aprendidos: {self.temas_aprendidos})"
 
 
-
-
+# Definición de la clase Profesor
 class Profesor:
-    def __init__(self,
-                 nombre: str,
-                 temas_dominados: list[str]
-                 ):
-        def __init__(self, nombre:str):
-            self.nombre = nombre
-            self.temas_dominados = []
+    def __init__(self, nombre: str, temas_dominados: list[str] = None):
+        self.nombre = nombre
+        self.temas_dominados = temas_dominados if temas_dominados is not None else []
 
-        def dominar_tema(self, tema:str)->None:
-            self.temas_aprendidos.append(tema)
-            print(f"{self.nombre} enseñó {tema}")
+    def dominar_tema(self, tema: str) -> None:
+        self.temas_dominados.append(tema)
+        print(f"{self.nombre} dominó el tema {tema}")
 
-        def teach_topic(self, no_tema:int) -> str:
-            self.temas_dominados[no_tema] = input("Ingresa el tema: ")
+    def teach_topic(self, no_tema: int) -> str:
+        tema = input("Ingresa el tema que vas a enseñar: ")
+        self.temas_dominados.append(tema)
+        print(f"{self.nombre} añadió el tema {tema} a su lista.")
+        if no_tema > len(self.temas_dominados):
             return self.temas_dominados[no_tema]
+        else:
+            return "Fuera de rango"
+
+
+    def __str__(self) -> str:
+        return f"Profesor (nombre: {self.nombre}, temas aprendidos: {self.temas_dominados})"
+
+
+if __name__ == '__main__':
+    estudiante1 = Estudiante("Yamm")
+    estudiante2 = Estudiante("Estudiante 2")
+    estudiante1.aprender_tema("Evolución sitios web")
+    estudiante2.aprender_tema("Internet de las cosas")
+    print()
+    print(estudiante1)
+    print()
+    print(estudiante2)
+    print()
+    profesor1 = Profesor("Profesor")
+    profesor1.dominar_tema("Clases")
+    profesor1.teach_topic(1)
+    print(profesor1)
+
+
 
 
---
+++ Codedex/Sorting_Hat.py
@@ -0,0 +1,64 @@
+# Write code below 💖
+
+Gryffindor = 0
+Ravenclaw = 0
+Hufflepuff = 0
+Slytherin = 0
+
+print("Do you like Dawn or Dusk?")
+print("    1) Dawn")
+print("    2) Dusk")
+q1 = int(input("Respuesta: "))
+
+if q1 == 1:
+    Gryffindor += 1
+    Ravenclaw += 1
+elif q1 == 2:
+    Hufflepuff += 1
+    Slytherin += 1
+else:
+    print("Wrong input.")
+
+
+print("When I’m dead, I want people to remember me as:")
+print("    1) The Good")
+print("    2) The Great")
+print("    3) The Wise")
+print("    4) The Bold")
+q2 = int(input("Respuesta: "))
+
+if q2 == 1:
+    Hufflepuff += 2
+elif q2 == 2:
+    Slytherin += 2
+elif q2 == 3:
+    Ravenclaw += 2
+elif q2 == 4:
+    Gryffindor += 2
+else:
+    print("Wrong input.")
+
+
+
+print("Which kind of instrument most pleases your ear?")
+print("    1) The violin")
+print("    2) The trumpet")
+print("    3) The piano")
+print("    4) The drum")
+q3 = int(input("Respuesta: "))
+if q3 == 1:
+    Slytherin += 4
+elif q3 == 2:
+    Hufflepuff += 4
+elif q3 == 3:
+    Ravenclaw += 4
+elif q3 == 4:
+    Gryffindor += 4
+else:
+    print("Wrong input.")
+
+print(f"Gryffindor {Gryffindor} points")
+print(f"Ravenclaw {Ravenclaw} points" )
+print(f"Hufflepuff {Hufflepuff} points")
+print(f"Slytherin {Slytherin} points")
+
