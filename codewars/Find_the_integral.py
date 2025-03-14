bfdf4af6c5797212f23aa165e0108218d068fda3
Duran Breceda <romeromatias273@gmail.com>
2025-03-14 14:22:28 -0600

    clases

--- codewars/Find_the_integral.py
+++ codewars/Find_the_integral.py
@@ -9,16 +9,33 @@ print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
 print(" ")
 
-def find_the_integral(coefficient_of_x : int, exponent: int) -> str:
+import unittest
+
+def integrate(coefficient, exponent) -> str:
     """
     :param coefficient_of_x: De tipo entero
     :param exponent: De tipo entero
     :return: Una cadena que muestra la integral
     """
-    return f"∫{coefficient_of_x}x^{exponent}dx = {coefficient_of_x/(exponent+1)}^{exponent+1} + C"
+    return f"∫{coefficient}x^{exponent}dx = {coefficient / (exponent + 1)}^{exponent + 1} + C"
+
+
+
+def basic_tests() -> None:
+    """
+
+    :return:
+    """
+    test.assert_equals(integrate(3, 2), "1x^3")
+    test.assert_equals(integrate(12, 5), "2x^6")
+    test.assert_equals(integrate(20, 1), "10x^2")
+    test.assert_equals(integrate(40, 3), "10x^4")
+    test.assert_equals(integrate(90, 2), "30x^3")
+
 
 
 if __name__ == '__main__':
-    coefficient_of_x = int(input("Ingresa el coeficiente de x en número positivo: "))
-    exponent = int(input("Ingresa el exponente en número positivo: "))
-    print(find_the_integral(coefficient_of_x, exponent))
+    basic_tests()
+
+
+
