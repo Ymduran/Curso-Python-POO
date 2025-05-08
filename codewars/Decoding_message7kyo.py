1e46b3da3bb5f0e067dfc0d6e9207ad23b117617
Durán Breceda Lourdes Jamileth <romeromatias273@gmail.com>
2025-05-07 21:30:30 -0600

    Decoding_message7kyo.py

---
+++ codewars/Decoding_message7kyo.py
@@ -0,0 +1,60 @@
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" * Durán Breceda Lourdes Jamileth                                   * ")
+print(" *                                                                  * ")
+print(" * Fecha: 07 de mayo del 2025                                       * ")
+print(" *                                                                  * ")
+print(" * Descripción:                                                     * ")
+print(" * Decodificar mensaje invirtiendo el alfabeto                      * ")
+print(" * Cada letra se cambia por su reflejo en el alfabeto.              * ")
+print(" * Por ejemplo, a <-> z, b <-> y, c <-> x, etc.                     * ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" ")
+print(" ")
+
+"""
+Función: decode(message)
+
+Descripción:
+Convierte un mensaje codificado invirtiendo cada letra en el alfabeto.
+El mensaje solo contiene letras minúsculas y espacios.
+"""
+
+def decode(message):
+    """
+    Decodifica un mensaje donde cada letra es sustituida por su reflejo en el alfabeto.
+
+    Parámetro:
+    message (str): El mensaje codificado en minúsculas y con espacios.
+
+    Retorna:
+    str: El mensaje decodificado.
+    """
+    alfabeto_original = "abcdefghijklmnopqrstuvwxyz"
+    alfabeto_invertido = "zyxwvutsrqponmlkjihgfedcba"
+    resultado = ""
+
+    i = 0
+    while i < len(message):
+        caracter = message[i]
+
+        if caracter == " ":
+            resultado = resultado + " "
+        else:
+            j = 0
+            while j < len(alfabeto_original):
+                if caracter == alfabeto_original[j]:
+                    resultado = resultado + alfabeto_invertido[j]
+                    break
+                j = j + 1
+
+        i = i + 1
+
+    return resultado
+
+
+# Pruebas
+if __name__ == '__main__':
+    print(decode("r slkv mlylwb wvxlwvh gsrh nvhhztv"))
+    # Resultado esperado: "i hope nobody decodes this message"
+    print(decode("zgyzhs rh ufm"))  # "attack is fun"
+    print(decode("abc xyz"))        # "zyx cba"
