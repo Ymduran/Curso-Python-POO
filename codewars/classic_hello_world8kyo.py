0e1dada329bbc28d0f558f4863a0df8af84afe40
Durán Breceda Lourdes Jamileth <romeromatias273@gmail.com>
2025-05-07 21:11:26 -0600

    classic_hello_world8kyo.py

---
+++ codewars/classic_hello_world8kyo.py
@@ -0,0 +1,24 @@
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" * Durán Breceda Lourdes Jamileth                                   * ")
+print(" *                                                                  * ")
+print(" * Fecha: 24 de marzo del 2025                                      * ")
+print(" *                                                                  * ")
+print(" * Descripción:                                                     * ")
+print(" * Mostrar en pantalla la frase 'Hello World!'                      * ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" ")
+print(" ")
+
+"""
+Escribe una clase llamada Solution que tenga un método llamado main.
+Este método debe imprimir en pantalla la frase "Hello World!" seguido de un salto de línea.
+"""
+
+class Solution:
+    def main(*parametros):
+        print("Hello World!")
+
+
+# Prueba del comportamiento esperado
+if __name__ == '__main__':
+    Solution.main("ejemplo1", "ejemplo2")
